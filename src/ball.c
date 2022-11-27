e8c119c5b9ffea409295cc25fcc8b5fc66157040
hbalint2100 <hbalint2100@gmail.com>
2022-11-27 01:37:44 +0100

    *Fixed bouncing

--- src/ball.c
+++ src/ball.c
@@ -26,12 +26,12 @@ void stepBall(ball *ball)
     ball->position = sumVector(ball->position,ball->direction_vector);
     if(ball->position.x + RAD >= WIDTH || ball->position.x - RAD < 0)
     {
-        ball->position.x *= -1;
         ball->direction_vector.x *= -1;
+        ball->position = sumVector(ball->position,ball->direction_vector);
     }
     if(ball->position.y + RAD >= HEIGHT || ball->position.y - RAD < 0)
     {
-        ball->position.y *= -1;
         ball->direction_vector.y *= -1;
+        ball->position = sumVector(ball->position,ball->direction_vector);
     }
 }
